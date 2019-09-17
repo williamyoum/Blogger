6c0ae8056ce0db04e41bc3af72a3513a8e791521
Will <will.youm@gmail.com>
2019-09-17 12:12:16 -0400

    blog comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
-
-# this file seems to be like urls.py
