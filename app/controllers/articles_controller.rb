6460b631866fd6927b07ccf9b9bd33d222c03b8f
Will <will.youm@gmail.com>
2019-09-17 11:05:46 -0400

    completen form-based workflow feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         # use instance variable that is not local
         # @variable allows the controller and view to access the var
@@ -7,6 +9,25 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 end
 
